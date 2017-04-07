bddd331386c4f741fd85c67be149635abaf73108
Samuel <sam@infinitely.io>
2017-04-06 21:40:13 -0600

    Update EditorLaunchSuccess component

--- components/EditorLaunchSuccess/index.js
+++ components/EditorLaunchSuccess/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 
 export default ({ err, children }) => !err ? <div>
   { children }
-  <style jsx>{`div { height: 100vh; width: 100vw; }`}</style>
+  <style jsx>{`div { height: 100vh; width: 100vw; display: flex; }`}</style>
 </div> : null;

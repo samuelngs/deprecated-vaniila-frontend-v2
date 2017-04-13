8e0a2e9e8dcc9d50044ec351c74714ef8383a2a3
Samuel <sam@infinitely.io>
2017-04-13 16:07:00 -0600

    Update new line handler for multiple line text

--- components/MomentCard/hooks/edit/NewLine.Replace.js
+++ components/MomentCard/hooks/edit/NewLine.Replace.js
@@ -12,9 +12,18 @@ const blockTemplate = {
   styles: [ ],
 };
 
+function findActualOffset(group, groups, offset) {
+  for ( let i = 0, c = offset; i < group && i < groups.length; i++ ) {
+    c += (groups[i] || '').length;
+    if ( i + 1 === group ) return c;
+  }
+  return offset;
+}
+
 export default function onNewLineReplace() {
 
-  const { moment, editorState } = this.props;
+  const { moment, editorState, root, id, onChange } = this.props;
+  const { store: { dispatch } } = this.context;
 
   const {
     editorStartKey,
@@ -37,8 +46,12 @@ export default function onNewLineReplace() {
   const clone = { };
   deepClone(clone, moment);
 
+  clone.hash = `${Date.now()}`;
+  clone.data = (clone.data || { });
+  clone.data.blocks = (clone.data.blocks || [ ]);
+
   // retrieve moment blocks
-  const blocks = (clone && clone.data && clone.data.blocks) || [ ];
+  const blocks = clone.data.blocks;
 
   let startBlock,
       startBlockIndex,
@@ -61,5 +74,117 @@ export default function onNewLineReplace() {
     if ( startBlock && endBlock ) break;
   }
 
-  console.log('new line replace', startBlock, endBlock);
+  // if two selected blocks (position) are actually the same block
+  const isBlocksEqual = startBlockIndex === endBlockIndex;
+
+  // count affected blocks;
+  // if start and end blocks are not the same block, deduct 1;
+  const affactedBlocksCount = endBlockIndex - startBlockIndex - (!isBlocksEqual ? 1 : 0);
+
+  const startStyleGroup = startStyleGroups[startOffsetGroup];
+  const endStyleGroup = endStyleGroups[endOffsetGroup];
+
+  if ( !startBlock || !endBlock || typeof startStyleGroup !== 'string' || typeof endStyleGroup !== 'string' ) return;
+
+  const actualStartOffset = findActualOffset(startOffsetGroup, startStyleGroups, editorStartOffset);
+  const actualEndOffset = findActualOffset(endOffsetGroup, endStyleGroups, editorEndOffset);
+
+  let textA, textB;
+  let newStartBlock, newEndBlock, newTextBlock;
+  const stylesA = [ ], stylesB = [ ];
+
+  // handle anything happens within the same block
+  if ( isBlocksEqual ) {
+
+    textA = startBlock.data.substr(0, actualStartOffset);
+    textB = startBlock.data.substr(actualEndOffset, startBlock.data.length);
+
+    if ( Array.isArray(startBlock.styles) ) {
+      for ( const { offset, length, style } of startBlock.styles ) {
+        const start = offset;
+        const end = offset + length;
+        if ( start >= actualEndOffset ) {
+          stylesB.push({ offset: 0, length, style });
+        } else if ( start < actualStartOffset ) {
+          let len = actualStartOffset - offset;
+          if ( len > length ) len = length;
+          stylesA.push({ offset, length: len, style });
+        }
+      }
+    }
+
+    if ( textA.length === 0 && textB.length === 0 ) {
+
+      newStartBlock = { ...blockTemplate, key: startBlock.key };
+      newTextBlock = { ...blockTemplate, key: UUID.v4() };
+
+    } else {
+
+      newStartBlock = { ...blockTemplate, key: startBlock.key, data: textA, styles: stylesA };
+      newTextBlock = { ...blockTemplate, key: UUID.v4(), data: textB, styles: stylesB };
+
+    }
+
+    blocks[startBlockIndex] = newStartBlock;
+    blocks.splice(startBlockIndex + 1, 0, newTextBlock);
+
+    return Promise.resolve(onChange(id, clone)).then(_ => {
+      return dispatch(api.setEditorState(root, {
+        anchorKey         : newTextBlock.key,
+        anchorGroup       : '0',
+        anchorOffset      : 0,
+        focusKey          : newTextBlock.key,
+        focusGroup        : '0',
+        focusOffset       : 0,
+        selectionRecovery : true,
+      }));
+    });
+
+  }
+
+  // handle anything happens equal or over two blocks
+
+  textA = startBlock.data.substr(0, actualStartOffset);
+  textB = endBlock.data.substr(actualEndOffset, endBlock.data.length);
+
+  if ( Array.isArray(startBlock.styles) ) {
+    for ( const { offset, length, style } of startBlock.styles ) {
+      const start = offset;
+      const end = offset + length;
+      if ( start < actualStartOffset ) {
+        let len = actualStartOffset - offset;
+        if ( len > length ) len = length;
+        stylesA.push({ offset, length: len, style });
+      }
+    }
+  }
+
+  if ( Array.isArray(endBlock.styles) ) {
+    for ( const { offset, length, style } of endBlock.styles ) {
+      const start = offset;
+      const end = offset + length;
+      if ( start >= actualEndOffset ) {
+        stylesB.push({ offset: 0, length, style });
+      }
+    }
+  }
+
+  newStartBlock = { ...startBlock, data: textA, styles: stylesA };
+  newEndBlock = { ...endBlock, data: textB, styles: stylesB };
+
+  blocks[startBlockIndex] = newStartBlock;
+  blocks[endBlockIndex] = newEndBlock;
+  if ( affactedBlocksCount > 0 ) blocks.splice(startBlockIndex + 1, affactedBlocksCount);
+
+  return Promise.resolve(onChange(id, clone)).then(_ => {
+    return dispatch(api.setEditorState(root, {
+      anchorKey         : endBlock.key,
+      anchorGroup       : '0',
+      anchorOffset      : 0,
+      focusKey          : endBlock.key,
+      focusGroup        : '0',
+      focusOffset       : 0,
+      selectionRecovery : true,
+    }));
+  });
 }

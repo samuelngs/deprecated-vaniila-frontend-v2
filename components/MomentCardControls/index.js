c09a4d9ea63b7e88dceefa9c73c7ce363485bd3e
Samuel <sam@infinitely.io>
2017-04-23 23:38:16 -0600

    Add moment card control menu and delete components

--- components/MomentCardControls/index.js
+++ components/MomentCardControls/index.js
@@ -2,20 +2,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Motion, spring } from 'react-motion';
+import { TransitionMotion, spring, presets } from 'react-motion';
 
 import Tooltip from '../Tooltip';
-import EditorMomentCardControlMedia from '../EditorMomentCardControlMedia';
+import EditorMomentCardControlDelete from '../EditorMomentCardControlDelete';
 import EditorMomentCardControlAlign from '../EditorMomentCardControlAlign';
+import EditorMomentCardControlMedia from '../EditorMomentCardControlMedia';
+
+const active   = [ { key: 'card-controls' } ];
+const inactive = [ ];
 
 export default class MomentCardControls extends React.Component {
 
   static propTypes = {
-    no        : PropTypes.number,
-    total     : PropTypes.number,
-    editmode  : PropTypes.bool,
-    active    : PropTypes.bool,
-    onAction  : PropTypes.func,
+    no          : PropTypes.number,
+    total       : PropTypes.number,
+    editmode    : PropTypes.bool,
+    active      : PropTypes.bool,
+    fullscreen  : PropTypes.bool,
+    onAction    : PropTypes.func,
   }
 
   static defaultProps = {
@@ -23,16 +28,11 @@ export default class MomentCardControls extends React.Component {
     total       : 0,
     editmode    : false,
     active      : false,
+    fullscreen  : false,
     onAction    : _ => null,
   }
 
-  constructor(props) {
-    super(props);
-    this.onFileChange = this.onFileChange.bind(this);
-    this.onFileClick = this.onFileClick.bind(this);
-  }
-
-  onFileChange(e) {
+  onFileChange = e => {
     e.preventDefault && e.preventDefault();
     const { target: { value, files } } = e;
     const { onAction } = this.props;
@@ -57,69 +57,106 @@ export default class MomentCardControls extends React.Component {
     });
   }
 
-  onFileClick(e) {
+  onFileClick = e => {
     e.target.value = null;
   }
 
-  render() {
-    const { no, total, editmode, active, onAction } = this.props;
-    return editmode && <Motion style={{ opacity: spring(active ? 1 : 0), y: spring(active ? -32 : -37) }}>
-      {({ opacity, y }) =><div className="base" style={{ opacity, transform: `translate3d(0px, ${y}px, 0px) scale(1)` }} data-controls>
-        <style jsx>{`
-          .base {
-            display: flex;
-            flex-direction: row;
-            position: absolute;
-            height: 18px;
-            padding-left: 10px;
-            padding-right: 10px;
-            left: 0;
-            right: 0;
-            color: #8a8a8a;
-            font-size: .9em;
-            font-weight: 300;
-          }
-          .column {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-          }
-          .column + .column {
-            margin-left: 14px;
-            padding-left: 14px;
-            border-left: 1px solid #e8e8e8;
-          }
-        `}</style>
-        <style jsx global>{`
-          .card-controls-button {
-            margin: 0;
-            padding: 0;
-            width: 18px;
-            height: 18px;
-            background-color: transparent;
-            border: none;
-            outline: none;
-          }
-          .card-controls-button + .card-controls-button {
-            margin-left: 10px;
-          }
-          .card-input-hidden {
-            position: absolute;
-            top: -3000px;
-          }
-        `}</style>
-        <div className="column" data-controls>{ no } of { total }</div>
-        <div className="column" data-controls>
-          <Tooltip tag="button" className="card-controls-button" title="Align" data-controls onClick={_ => onAction('align-moment')}>
-            <EditorMomentCardControlAlign />
-          </Tooltip>
-          <Tooltip tag="label" className="card-controls-button" title="Upload image" data-controls>
-            <EditorMomentCardControlMedia />
-            <input className="card-input-hidden" type="file" onChange={this.onFileChange} onClick={this.onFileClick} data-controls />
-          </Tooltip>
-        </div>
+  willEnter() {
+    return { opacity: 0, y: -37 }
+  }
+
+  willLeave() {
+    return { opacity: spring(0), y: spring(-37) }
+  }
+
+  getDefaultStyles() {
+    const arr = this.props.active
+      ? active
+      : inactive;
+    return arr.map(o => ({ ...o, style: { opacity: 0, y: -37 } }));
+  }
+
+  getStyles() {
+    const arr = this.props.active
+      ? active
+      : inactive;
+    return arr.map(o => ({ ...o, style: { opacity: spring(1), y: spring(-32) } }));
+  }
+
+  renderCardControls = ({ key, style, data }) => {
+    const { no, total, editmode, active, fullscreen, onAction } = this.props;
+    return <div key={key} className="base" style={{ opacity: style.opacity, transform: `translate3d(0px, ${style.y}px, 0px) scale(1)` }} data-controls>
+      <style jsx>{`
+        .base {
+          display: flex;
+          flex-direction: row;
+          position: absolute;
+          height: 18px;
+          padding-left: 10px;
+          padding-right: 10px;
+          left: 0;
+          right: 0;
+          color: #8a8a8a;
+          font-size: .9em;
+          font-weight: 300;
+          z-index: 1;
+        }
+        .column {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        }
+        .column + .column {
+          margin-left: 14px;
+          padding-left: 14px;
+          border-left: 1px solid #e8e8e8;
+        }
+      `}</style>
+      <style jsx global>{`
+        .card-controls-button {
+          margin: 0;
+          padding: 0;
+          width: 18px;
+          height: 18px;
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+          outline: none;
+        }
+        .card-controls-button + .card-controls-button {
+          margin-left: 10px;
+        }
+        .card-input-hidden {
+          position: absolute;
+          top: -3000px;
+        }
+      `}</style>
+      <div className="column" data-controls>{ no } of { total }</div>
+      { !fullscreen && <div className="column" data-controls>
+        <Tooltip tag="button" className="card-controls-button" title="Align" data-controls onClick={_ => onAction('align-moment')}>
+          <EditorMomentCardControlAlign />
+        </Tooltip>
+        <Tooltip tag="label" className="card-controls-button" title="Upload image" data-controls>
+          <EditorMomentCardControlMedia />
+          <input className="card-input-hidden" type="file" onChange={this.onFileChange} onClick={this.onFileClick} data-controls />
+        </Tooltip>
       </div> }
-    </Motion>
+      { total > 1 && <div className="column" data-controls>
+        <Tooltip tag="button" className="card-controls-button" title="Delete moment" data-controls onClick={_ => onAction('delete-moment')}>
+          <EditorMomentCardControlDelete />
+        </Tooltip>
+      </div> }
+    </div>
+  }
+
+  render() {
+    return <TransitionMotion
+      defaultStyles={this.getDefaultStyles()}
+      styles={this.getStyles()}
+      willLeave={::this.willLeave}
+      willEnter={::this.willEnter}>
+      { styles => styles.length > 0 ? this.renderCardControls(styles[0]) : null }
+    </TransitionMotion>
   }
 
 }

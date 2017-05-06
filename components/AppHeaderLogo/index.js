17597696ffe4937380e07d84dfcedfd938185bc0
Samuel <sam@infinitely.io>
2017-05-06 03:30:15 -0600

    Change application logo color

--- components/AppHeaderLogo/index.js
+++ components/AppHeaderLogo/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 export default ({ headerHeight = 45 }) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" style={{ height: headerHeight, width: headerHeight }}>
   <style jsx>{`
-    .st0 { fill: #185BE7; }
-    .st1 { fill: #59b7ff; }
-    .st2 { fill: #c2f2ff; }
+    .st0 { fill: #cacaca; }
+    .st1 { fill: #ddd; }
+    .st2 { fill: #f3f3f3; }
   `}</style>
   <path d="M45 75.7h-5c-2.8 0-5.2-2.3-5.2-5.2 0-2.8 2.3-5.2 5.2-5.2h5c2.8 0 5.2 2.3 5.2 5.2 0 2.8-2.3 5.2-5.2 5.2z" className="st0"/>
   <path d="M109.8 75.7H63.1c-2.8 0-5.2-2.3-5.2-5.2 0-2.8 2.3-5.2 5.2-5.2h46.7c2.8 0 5.2 2.3 5.2 5.2-.1 2.8-2.4 5.2-5.2 5.2z" className="st1"/>

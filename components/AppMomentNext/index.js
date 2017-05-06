b365d1175350fcbdaf9d584b0df081cb3294db9e
Samuel <sam@infinitely.io>
2017-05-06 01:53:03 -0600

    Add moments navigation component

---
+++ components/AppMomentNext/index.js
@@ -0,0 +1,102 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { TransitionMotion, spring } from 'react-motion';
+
+const active   = [ { key: 'control' } ];
+const inactive = [ ];
+
+export default class AppMomentNext extends React.Component {
+
+  static propTypes = {
+    active  : PropTypes.bool,
+    onPress : PropTypes.func,
+  }
+
+  static defaultProps = {
+    active  : false,
+    onPress : null,
+  }
+
+  willEnter = o => {
+    return { opacity: 0 }
+  }
+
+  willLeave = o => {
+    return { opacity: spring(0) }
+  }
+
+  getDefaultStyles = o => {
+    const { active: visible } = this.props;
+    const arr = visible
+      ? active
+      : inactive;
+    return arr.map(o => ({ ...o, style: { opacity: 0 } }));
+  }
+
+  getStyles = o => {
+    const { active: visible } = this.props;
+    const arr = visible
+      ? active
+      : inactive;
+    return arr.map(o => ({ ...o, style: { opacity: spring(1) } }));
+  }
+
+  render() {
+    const { onPress } = this.props;
+    return <TransitionMotion
+      defaultStyles={this.getDefaultStyles()}
+      styles={this.getStyles()}
+      willLeave={this.willLeave}
+      willEnter={this.willEnter}>
+      { styles => styles.length > 0 ? <button className="button" onClick={onPress} style={{ opacity: styles[0].style.opacity }}>
+        <style jsx>{`
+          .button {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            right: 0px;
+            margin-top: 0;
+            margin-bottom: 0;
+            margin-left: 0;
+            margin-right: 0;
+            padding-top: 0;
+            padding-bottom: 0;
+            padding-left: 14px;
+            padding-right: 14px;
+            background-color: transparent;
+            border: none;
+            cursor: pointer;
+            outline: none;
+            display: none;
+          }
+          .svg {
+            margin-top: 0;
+            margin-bottom: 0;
+            margin-left: 0;
+            margin-right: 0;
+            padding-top: 0;
+            padding-bottom: 0;
+            padding-left: 0;
+            padding-right: 0;
+            width: 24px;
+            height: 24px;
+            fill: #aaa;
+          }
+          @media (min-width: 768px) {
+            .button {
+              display: initial;
+            }
+          }
+        `}</style>
+        <svg className="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <path d="M28.003 5.984a2 2 0 0 1 1.388.572l44 42a2 2 0 0 1 0 2.894l-44 42a2 2 0 1 1-2.76-2.894l42.485-40.553L26.63 9.45a2 2 0 0 1 1.373-3.466z"/>
+        </svg>
+      </button> : null }
+    </TransitionMotion>
+  }
+
+}
+
+

01ea4a9e75f6efca29149e92434372d7e05fa887
Samuel <sam@infinitely.io>
2017-05-17 09:55:46 -0600

    Add tracking code

--- storage/redux.js
+++ storage/redux.js
@@ -27,6 +27,37 @@ if (
   window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {};
 }
 
+if (
+  process.env.NODE_ENV === 'production' &&
+  typeof window !== 'undefined'
+) {
+
+  // google analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-85759384-1', 'auto');
+  ga('send', 'pageview');
+
+  // fullstory
+  window['_fs_debug'] = false;
+  window['_fs_host'] = 'fullstory.com';
+  window['_fs_org'] = '4MA4X';
+  window['_fs_namespace'] = 'FS';
+  (function(m,n,e,t,l,o,g,y){
+    if (e in m && m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].'); return;}
+    g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
+    o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+    y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+    g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};
+    g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+    g.clearUserCookie=function(c,d,i){if(!c || document.cookie.match('fs_uid=[`;`]*`[`;`]*`[`;`]*`')){
+    d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+
+    ';path=/;expires='+new Date(0).toUTCString();i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}};
+  })(window,document,window['_fs_namespace'],'script','user');
+}
+
 /**
  * redux store initialization
  */
@@ -240,7 +271,18 @@ export default function(createStore) {
 
       componentDidMount() {
         Router.onRouteChangeStart = path => {
+          const { accountUsername, accountFullname } = this.store.getState();
           this.store.dispatch({ type: actions.SetServerPath, path });
+          if ( typeof window !== 'undefined' && typeof window.ga === 'function' && !!accountUsername ) {
+            ga('set', 'userId', accountUsername);
+            ga('set', 'page', path);
+            ga('send', 'pageview');
+          }
+          if ( typeof window !== 'undefined' && typeof window.FS === 'object' && !!accountUsername ) {
+            FS.identify(accountUsername, {
+              displayName: accountFullname,
+            });
+          }
         }
       }
 

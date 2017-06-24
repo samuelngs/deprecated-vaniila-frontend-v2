2eeb4727f99b164f7986a56a84c29f47cd876995
Samuel <sam@infinitely.io>
2017-06-24 14:44:33 -0600

    Show header in sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import Head from 'next/head';
 
+import WindowObserver from '../components/WindowObserver';
+import BackToTop from '../components/BackToTop';
+import AppHeader from '../components/AppHeader';
 import AppModalSignin from '../components/AppModalSignin';
 
 import withRedux from '../storage';
@@ -17,20 +20,45 @@ class SignIn extends React.Component {
   }
 
   render () {
-    return <div className="container">
+    return <div>
       <style jsx>{`
         .container {
-          display: flex;
-          width: 100vw;
-          height: 100vh;
-          justify-content: center;
-          align-items: center;
+          margin-top: 0;
+          margin-bottom: 0;
+          margin-left: auto;
+          margin-right: auto;
+          padding-top: 80px;
+          padding-bottom: 130px;
+          padding-left: 0;
+          padding-right: 0;
+          width: 300px;
+          max-width: 100%;
+          max-width: calc(100% - 40px);
+        }
+        @media (min-width: 780px) {
+          .container {
+            width: 700px;
+          }
+        }
+        @media (min-width: 1160px) {
+          .container {
+            width: 1080px;
+          }
         }
       `}</style>
+
       <Head>
         <title>Sign In</title>
       </Head>
-      <AppModalSignin />
+
+      <BackToTop id="sign-in" />
+      <WindowObserver />
+
+      <AppHeader />
+
+      <div className="container">
+        <AppModalSignin />
+      </div>
     </div>
   }
 
